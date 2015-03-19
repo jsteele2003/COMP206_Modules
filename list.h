f8b11e759bdb12fef4898acd96a246b7cddbf36b
Joe Steele <joe.steele60@gmail.com>
2015-03-18 22:55:52 -0400

    commit:first function prototypes

--- list.h
+++ list.h
@@ -1,1 +1,14 @@
-#include <iostream>
+//Joe Steele, 260516386
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <malloc.h>
+
+
+typedef struct node {
+    int val;
+    struct node * next;
+} inode;
+
+void add(int){};
